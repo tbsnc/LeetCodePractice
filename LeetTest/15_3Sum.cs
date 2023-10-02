65122d29fa722281bb37a97c190a8db192d60ae8
tbsnc <tomislav.bosnic@yahoo.com>
2023-10-02 02:30:15 +0200

    #16 ThreeSumClosest DONE

--- LeetTest/15_3Sum.cs
+++ LeetTest/15_3Sum.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+
 
 namespace LeetTest
 {
@@ -8,36 +10,82 @@ namespace LeetTest
         public _15_3Sum(int[] nums)
         {
             // ThreeSum(nums);
-            ThreeSumClosest(new int[] { 40, -53, 36, 89, -38, -51, 80, 11, -10, 76, -30, 46, -39, -15, 4, 72, 83, -25, 33, -69, -73, -100, -23, -37, -13, -62, -26, -54, 36, -84, -65, -51, 11, 98, -21, 49, 51, 78, -58, -40, 95, -81, 41, -17, -70, 83, -88, -14, -75, -10, -44, -21, 6, 68, -81, -1, 41, -61, -82, -24, 45, 19, 6, -98, 11, 9, -66, 50, -97, -2, 58, 17, 51, -13, 88, -16, -77, 31, 35, 98, -2, 0, -70, 6, -34, -8, 78, 22, -1, -93, -39, -88, -77, -65, 80, 91, 35, -15, 7, -37, -96, 65, 3, 33, -22, 60, 1, 76, -32, 22 }, 292 );
+            ThreeSumClosestMuchFaster(new int[] { -1, 2, 1, -4 }, 1);
+            ThreeSumClosestMuchFaster(new int[] { 833, 736, 953, -584, -448, 207, 128, -445, 126, 248, 871, 860, 333, -899, 463, 488, -50, -331, 903, 575, 265, 162, -733, 648, 678, 549, 579, -172, -897, 562, -503, -508, 858, 259, -347, -162, -505, -694, 300, -40, -147, 383, -221, -28, -699, 36, -229, 960, 317, -585, 879, 406, 2, 409, -393, -934, 67, 71, -312, 787, 161, 514, 865, 60, 555, 843, -725, -966, -352, 862, 821, 803, -835, -635, 476, -704, -78, 393, 212, 767, -833, 543, 923, -993, 274, -839, 389, 447, 741, 999, -87, 599, -349, -515, -553, -14, -421, -294, -204, -713, 497, 168, 337, -345, -948, 145, 625, 901, 34, -306, -546, -536, 332, -467, -729, 229, -170, -915, 407, 450, 159, -385, 163, -420, 58, 869, 308, -494, 367, -33, 205, -823, -869, 478, -238, -375, 352, 113, -741, -970, -990, 802, -173, -977, 464, -801, -408, -77, 694, -58, -796, -599, -918, 643, -651, -555, 864, -274, 534, 211, -910, 815, -102, 24, -461, -146 }, -7111);
+
         }
 
-        public int ThreeSumClosest(int[] nums, int target) //16 three sum closest TODO
-        {
-            List<int> listSums = new List<int>();
-            int l, r;
 
+        public int ThreeSumClosestMuchFaster(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            int current;
+            int solution = int.MaxValue;
+       
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+               int l = i + 1;
+               int  r = nums.Length - 1;
+
+                while (l < r)
+                {
+                    current = nums[i] + nums[l] + nums[r];
+
+                    if (current == target)
+                    {
+                        return current;
+                    }
+                    else if (current < target)
+                    {
+                        l++;
+                    }
+                    else { r--; }
+
+                    if (Math.Abs(current - target) < Math.Abs(solution - target))
+                    {
+                        solution = current;
+                    }
+                }
+            }
+            return solution;
+        }
+
+        public int ThreeSumClosest(int[] nums, int target) //16 three sum closest 
+        {
+            Dictionary<int,int> dictOfSums = new Dictionary<int, int>();
+            int l, r;
+            int current = 0;
+            int closest = int.MaxValue;
             for (int i = 0; i < nums.Length; i++)
             {
             
                 l = i + 1;
                 r = nums.Length - 1;
 
-                while (l < r)
+                for (l = i + 1; l < nums.Length - 1; l++)
                 {
-                    listSums.Add(nums[i] + nums[l] + nums[r]);
-                    r--;
+                    while (l < r)
+                    {
+                        current = Math.Abs((nums[i] + nums[l] + nums[r]) - target);
+                        
+                        if (current < closest)
+                        {
+                            dictOfSums[current] = nums[i] + nums[l] + nums[r];
+                        }
+                        
+                        r--;
+                    }
+                    r = nums.Length - 1;
                 }
-
 
             }
 
-            if(listSums.Any(x => x == target)) return listSums.Where(x => x == target).FirstOrDefault();  
-            int sol1 = listSums.Where(x => x < target).DefaultIfEmpty().Max();
-            int sol2 = listSums.Where(x => x > target).DefaultIfEmpty().Min();
-
            
 
-            return sol1 > sol2 ? sol1 : sol2;
+
+            return dictOfSums[dictOfSums.Min(x => x.Key)];
+            
 
         }
 
